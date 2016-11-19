1c4d7f4acc58a2d73c7ce4317c83c5425b9a4989
bardeen <you@example.com>
2016-11-19 10:25:24 +0100

    asd

--- pg4.py
+++ pg4.py
@@ -1,45 +1,95 @@
 import psycopg2
 import sqlite3
 import sys, math
+import triangulate
 
-sq = sqlite3.connect('/sit2d_2.db')
+# -*- coding: utf-8 -*-
+
+sq = sqlite3.connect('sit2d_4.db')
 sqc = sq.cursor()
+#conn = psycopg2.connect("host=dell dbname=gis user=florian")
 conn = psycopg2.connect("dbname=gis user=florian")
 cur = conn.cursor()
 
 sqc.execute("delete from vertex")
 sqc.execute("delete from polygon")
 sqc.execute("delete from eigenschaften")
+sqc.execute("delete from residential")
 
-sqc.execute("vacuum")
 sq.commit()
 
-def toDB(data, p_id, p_lvl):
+
+def toDB(data, p_id, p_lvl, p_table):
 	points = data.split(",")
 	seq = 0
-	ll = [sys.float_info.max,0,sys.float_info.max,0]
-
+	old_point = []
 	for point in points:	
 		try:
 			t = point.split(" ")
 			g_x = (float(t[0]) + 180.0) / 360.0 * pow2Z;
 			g_y = (1.0 - math.log(math.tan(float(t[1]) * rad2deg) + 1.0 / math.cos(float(t[1]) * rad2deg)) / math.pi) / 2.0 * pow2Z;
-			sqc.execute("insert into polygon(V_L_ID,V_SEQ,V_LON,V_LAT,V_LVL) values (?,?,?,?,?)", (p_id,seq,g_x,g_y,p_lvl))
-			if g_x < ll[0]:
-				ll[0] = g_x
-			if g_x > ll[1]:
-				ll[1] = g_x
-			if g_y < ll[2]:
-				ll[2] = g_y
-			if g_y > ll[3]:
-				ll[3] = g_y
+
+			if (seq % 2) == 0 and seq > 0:
+				#print "-> " + str(old_point[0]) + " " + str(old_point[1])
+				sqc.execute("insert into " + p_table + "(V_L_ID,V_SEQ,V_LON,V_LAT) values (?,?,?,?)", (p_id,seq,old_point[0],old_point[1]))
+				seq = seq + 1
+			#else:
+				#print "erster"
+
+			#print "   " + str(g_x) + " " + str(g_y)
+			sqc.execute("insert into " + p_table + "(V_L_ID,V_SEQ,V_LON,V_LAT) values (?,?,?,?)", (p_id,seq,g_x,g_y))
 			
+			old_point = [g_x,g_y]
 			seq = seq + 1
 		except:
 			print "Unexpected error:", sys.exc_info()[0]
 			#print row			
 			raise
-	return ll
+	return seq
+
+def toDB2(data, p_id, p_lvl, p_table):
+	points = data.split(",")
+
+	vert = [[4, 3]]
+	vert.pop()
+
+	for point in points:
+		#print point	
+		try:
+			t = point.split(" ")
+			g_x = (float(t[0]) + 180.0) / 360.0 * pow2Z;
+			g_y = (1.0 - math.log(math.tan(float(t[1]) * rad2deg) + 1.0 / math.cos(float(t[1]) * rad2deg)) / math.pi) / 2.0 * pow2Z;
+
+			vert.append([g_x,g_y])
+		except:
+			print "Unexpected error:", sys.exc_info()[0]
+			#print row			
+			raise
+
+	seq = 0	
+	tri = []
+	plist = vert[::-1] if triangulate.IsClockwise(vert) else vert[:]
+	plist.pop()
+	
+	while len(plist) >= 3:
+		a = triangulate.GetEar(plist)
+		if a == []:
+			break	
+		sqc.execute("insert into " + p_table + "(V_L_ID,V_SEQ,V_LON,V_LAT) values (?,?,?,?)", (p_id,seq,a[0][0],a[0][1]))
+		sqc.execute("insert into " + p_table + "(V_L_ID,V_SEQ,V_LON,V_LAT) values (?,?,?,?)", (p_id,seq+1,a[1][0],a[1][1]))
+		sqc.execute("insert into " + p_table + "(V_L_ID,V_SEQ,V_LON,V_LAT) values (?,?,?,?)", (p_id,seq+2,a[2][0],a[2][1]))
+		seq = seq + 3	
+		tri.append(a)
+	#print ''
+	#for point in tri:
+	#	print point
+	#	print str(point[0][0]) + ", " + str(point[0][1])
+	#	print str(point[1][0]) + ", " + str(point[1][1])
+	#	print str(point[2][0]) + ", " + str(point[2][1])
+		#print 
+
+	return seq
+
 
 def asd(ert, p_id, lvl):
 	if len(ert) == 0:
@@ -78,76 +128,43 @@ def progressbar(header,progress):
 		print ""
 	sys.stdout.flush()
 
-
-
 linie = 0
 pow2Z = pow(2.0, 18);
 rad2deg = math.pi / 180.0;
 
-vert = [[4, 3]]
-vert.pop()
 
-lat = 52.0611
-lon = 11.7229
-lld =  0.1
+
+#lat = 52.0611
+#lon = 11.7229
+#lld =  0.1
 
 sql = "select ST_AsText(way),osm_id,typ from v_line"
-sql = sql + " where way && ST_MakeEnvelope(" + str(lon-lld) + ", " + str(lat-lld) + ", " + str(lon+lld) + ", " + str(lat+lld) + ", 4326)"
-#sql = "select ST_AsText(ST_Transform(way,4326)),osm_id from planet_osm_line where 1=1"
+#sql = sql + " where way && ST_MakeEnvelope(" + str(lon-lld) + ", " + str(lat-lld) + ", " + str(lon+lld) + ", " + str(lat+lld) + ", 4326)"
+#sql = sql + " where osm_id in (92116198, 33955912)"
 cur.execute(sql)
 
 x = cur.fetchall()
 verts=float(cur.rowcount)
 
 for row in x:
-	if row[2] in ('service','residential'):
-		r = 1.0
-		g = 1.0
-		b = 1.0
-	elif row[2] == 'secondary':
-		r = 0.965
-		g = 0.976
-		b = 0.745
-	elif row[2] == 'primary':
-		r = 0.984
-		g = 0.835
-		b = 0.643
-	elif row[2] == 'trunk':
-		r = 0.972
-		g = 0.698
-		b = 0.612
-	elif row[2] == 'track':
-		r = 0.337
-		g = 0.274
-		b = 0.254
-	elif row[2] in ('motorway','motorway_link'):
-		r = 1.0
-		g = 0.1
-		b = 0.1
-	else:
-		r = 1.0
-		g = 0.0
-		b = 0.0
+	tmp=row[0][:-2].replace("LINESTRING(","")
 
-	points = row[0].replace(")","").replace("LINESTRING(","").split(",")
+	if tmp.find("(") == -1:
+		line_len = toDB(tmp,linie, 1, "vertex")
+		sql = "insert into eigenschaften(L_ID,OSM_ID,L_COUNT,L_TYP) values (?,?,?,?)"
+		try:
+			typ = unicode(row[2])
+		except:
+			typ = unicode()	
+		sqc.execute(sql, (linie,row[1],line_len,typ ))
+#hashlib.md5(str(row[2])).digest().encode('base64')[:4]  ))	
 
-	sqc.execute("insert into eigenschaften(L_ID,L_F_R,L_F_G,L_F_B,OSM_ID) values (?,?,?,?,?)", (linie,r,g,b,row[1]))
-
-	n2 = 0
-	for point in points:
-		t = point.split(" ")
-		g_x = (float(t[0]) + 180.0) / 360.0 * pow2Z;
-		g_y = (1.0 - math.log(math.tan(float(t[1]) * rad2deg) + 1.0 / math.cos(float(t[1]) * rad2deg)) / math.pi) / 2.0 * pow2Z;
-
-		sqc.execute("insert into vertex(V_L_ID,V_SEQ,V_LON,V_LAT) values (?,?,?,?)", (linie,n2,g_x,g_y))
-		n2 = n2 + 1
-	
 	linie = linie + 1
-	progressbar("linien",linie/verts)
-	
-sql = "select ST_AsText(way),osm_id,typ from v_polygon t"
-sql = sql + " where way && ST_MakeEnvelope(" + str(lon-lld) + ", " + str(lat-lld) + ", " + str(lon+lld) + ", " + str(lat+lld) + ", 4326)"
-#sql = sql + " where osm_id=134149361"
+	progressbar("linien ",linie/verts)
+
+sql = "select ST_AsText(way),osm_id,typ from v_building t"
+#sql = sql + " where way && ST_MakeEnvelope(" + str(lon-lld) + ", " + str(lat-lld) + ", " + str(lon+lld) + ", " + str(lat+lld) + ", 4326)"
+#sql = sql + " where osm_id = 26879098"
 cur.execute(sql)
 
 x = cur.fetchall()
@@ -156,110 +173,62 @@ poly=float(cur.rowcount)
 leer = []
 
 for row in x:
-	if row[2] in ('water','riverbank'):
-		r = 0.0
-		g = 0.0
-		b = 1.0
-	elif row[2] in ('forest','wood'):
-		r = 0.13
-		g = 0.54
-		b = 0.13
-	elif row[2] == 'farmyard':
-		r = 0.91
-		g = 0.85
-		b = 0.76
-	elif row[2] == 'farmland':
-		r = 0.95
-		g = 0.92
-		b = 0.86
-	elif row[2] == 'residential':
-		r = 0.878
-		g = 0.87
-		b = 0.87
-	elif row[2] == 'building':
-		r = 0.847
-		g = 0.82
-		b = 0.79
-	elif row[2] == 'scree':
-		r = 0.80
-		g = 0.78
-		b = 0.45
-	elif row[2] == 'meadow':
-		r = 0.80
-		g = 0.92
-		b = 0.69
-	elif row[2] == 'public transport':
-		r = 1.0
-		g = 0.0
-		b = 0.0
-	elif row[2] == 'scrub':
-		r = 0.72
-		g = 0.89
-		b = 0.72
-	elif row[2] == 'park':
-		r = 0.72
-		g = 0.89
-		b = 0.72
-	elif row[2] == 'allotments':
-		r = 0.90
-		g = 0.78
-		b = 0.67
-	elif row[2] == 'landfill':
-		r = 0.71	
-		g = 0.71
-		b = 0.57
-	elif row[2] == 'wetland':
-		r = 0.80
-		g = 0.92
-		b = 0.698
-	elif row[2] == 'playground':
-		r = 0.80
-		g = 1.0
-		b = 0.94
-	elif row[2] == 'parking':
-		r = 0.965
-		g = 0.933
-		b = 0.714
-	elif row[2] == 'dog_park':
-		r = 0.784
-		g = 0.976
-		b = 0.8
-	elif row[2] == 'track':
-		r = 0.455
-		g = 0.863
-		b = 0.729
-	elif row[2] == 'pitch':
-		r = 0.537
-		g = 0.824
-		b = 0.682
-	else:
-		try:
-			leer.index(row[2])
-		except:
-			leer.append(row[2])
-		r = 1.0
-		g = 0.0
-		b = 0.0
-	#asd(row[0].replace("))",")").replace("POLYGON((","("), linie,0)
-	#tmp="11 22,(33 44)"
 	tmp=row[0][:-2].replace("POLYGON((","")
-	try:
-		if tmp.find("(") == -1:
-			ll=toDB(tmp,linie, 1)
-			sqc.execute("insert into eigenschaften(L_ID,L_F_R,L_F_G,L_F_B,OSM_ID,L_LON1,L_LON2,L_LAT1,L_LAT2) values (?,?,?,?,?,?,?,?,?)", (linie,r,g,b,row[1],ll[0],ll[1],ll[2],ll[3]))
-		
-	except:
-		print "asd"
-		continue
-		
+	if tmp.find("(") == -1 and row[0] <> 'POLYGON EMPTY':
+		tris = toDB2(tmp,linie, 1, "polygon")	
+		#len(tmp.split(","))
 
+		sql = "insert into eigenschaften(L_ID,OSM_ID,L_COUNT,L_TYP) values (?,?,?,?)"
+		try:
+			typ = unicode(row[2])
+		except:
+			typ = unicode()	
+		sqc.execute(sql, (linie,row[1],tris,typ ))
+		
+	
 	linie = linie + 1
 	progressbar("polygon",(linie-verts)/poly)
 	
-#print leer
+sql = "select ST_AsText(way),osm_id,typ from v_residential t"
+#sql = sql + " where osm_id=26879098"
+cur.execute(sql)
+
+x = cur.fetchall()
+resi=float(cur.rowcount)
+
+leer = []
+
+for row in x:
+	tmp=row[0][:-2].replace("POLYGON((","")
+	if tmp.find("(") == -1 and row[0] <> 'POLYGON EMPTY':
+		tris = toDB2(tmp,linie, 1, "residential")	
+		#len(tmp.split(","))
+
+		sql = "insert into eigenschaften(L_ID,OSM_ID,L_COUNT,L_TYP) values (?,?,?,?)"
+		try:
+			typ = unicode(row[2])
+		except:
+			typ = unicode()	
+		sqc.execute(sql, (linie,row[1],tris,typ ))
+
+	linie = linie + 1
+	progressbar("residential",(linie-verts-poly)/resi)
+
+#sql = "select distinct typ from v_polygon union all select distinct typ from v_line union all select distinct typ from v_residential"
+#cur.execute(sql)
+#x = cur.fetchall()
+#for row in x:
+	#sql = "update config_farbe set bezeichnung = ? where cf_id=?"
+	#sqc.execute(sql, (row[0],hashlib.md5(str(row[0])).digest().encode('base64')[:4]  ))
 
 cur.close()
 conn.close()
-
 sq.commit()
+
+sqc.execute("insert into config_farbe (CF_ID,R,G,B) select distinct L_TYP,1,0,0 from eigenschaften where not exists (select * from config_farbe where CF_ID=L_TYP) and L_TYP is not null");
+#sqc.execute("delete FROM polygon WHERE EXISTS (SELECT * FROM eigenschaften WHERE V_L_ID=L_ID AND L_COUNT-1=V_SEQ)");
+#sqc.execute("delete FROM residential WHERE EXISTS (SELECT * FROM eigenschaften WHERE V_L_ID=L_ID AND L_COUNT-1=V_SEQ)");
+#sqc.execute("UPDATE eigenschaften SET L_COUNT=L_COUNT-1 WHERE EXISTS (SELECT * FROM polygon WHERE V_L_ID = L_ID)");
+
+sqc.execute("vacuum")
 sqc.close()
